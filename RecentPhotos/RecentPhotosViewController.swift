7b7b0ae9aa3d5d6c285c1a469c096ddd91e6bbb1
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2019-04-06 18:22:14 +0300

    Initialization

---
+++ RecentPhotos/RecentPhotosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecentPhotos
+//
+//  Created by Ulaş Sancak on 6.04.2019.
+//  Copyright © 2019 Ulaş Sancak. All rights reserved.
+//
+
+import UIKit
+
+class RecentPhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
